8fa7b86f226565584aac031fbd5dd47e1251a395
Tom Crayford <tcrayford@googlemail.com>
2010-05-15 19:46:28 +0100

    reworked the whole thing to use comp.

--- src/lychrels/core.clj
+++ src/lychrels/core.clj
@@ -3,32 +3,29 @@
   (:gen-class))
 
 (defn reverse-int
-  "returns an integer with its digits reversed, e.g. 123 returns 321"
+  "Returns an integer with its digits reversed, e.g. 123 returns 321"
   [x]
   (BigInteger. (su/reverse (pr-str x))))
 
 (defn palindromic?
-  "returns true if the number is palindromic, e.g. 121, 1234321"
+  "Returns true if the number is palindromic, e.g. 121, 1234321"
   [x]
-  (= x (reverse-int x))
-  )
+  (= x (reverse-int x)))
 
 (defn lychrel?
-  "returns true if the number recursively added to its reverse is not
-   a palindrome after 50 iterations"
   ([x] (lychrel? x 50))
   ([x iterations]
-     (let [reverse-sum (+ x (reverse-int x))]
-       (cond (< iterations 1) true
-             (palindromic? reverse-sum) false
-             :default (lychrel? reverse-sum (dec iterations)))))
-  )
+     (let [next-num #(+ % (reverse-int %))]
+       ((comp not
+              #(some palindromic? %)
+              (partial take iterations)
+              rest
+              #(iterate next-num %)) x))))
 
 (defn count-lychrels-to-10000
   "Counts the number of lychrels below 10000"
   []
-  (count (filter lychrel? (range 1 10000)))
-  )
+  (count (filter lychrel? (range 1 10000))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
